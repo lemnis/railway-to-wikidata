4c177652a4c6174fd8c814d4208319c6c5614da6
Lisa Martens <lisa@whalar.com>
2022-01-31 20:33:35 +0100

    Add obb gtfs feed

---
+++ src/providers/obb/index.ts
@@ -0,0 +1,31 @@
+import { CodeIssuer, Property } from "../../types/wikidata";
+import { LocationV4 } from "../../types/location";
+import { findCountryByUIC } from "../../transform/country";
+import { getGtfsStations } from "../../utils/gtfs";
+
+/**
+ * @version 2018.06.26
+ * @license CC BY 4.0
+ * @see https://data.oebb.at/oebb
+ */
+export const getLocations = (): Promise<LocationV4[]> =>
+  getGtfsStations(
+    "https://data.oebb.at/oebb?dataset=uddi:cd36722f-1b9a-11e8-8087-b71b4f81793a&file=uddi:d3e25791-7889-11e8-8fc8-edb0b0e1f0ef/GFTS_Fahrplan_OEBB.zip",
+    "obb"
+  ).then((data) =>
+    data.map(({ stop_lat, stop_lon, stop_name, stop_id }) => {
+      return {
+        id: stop_id,
+        labels: [{ value: stop_name }],
+        claims: {
+          [CodeIssuer.UIC]: [{ value: stop_id }],
+          [Property.Country]: [
+            { value: findCountryByUIC(parseInt(stop_id))?.wikidata },
+          ],
+          [Property.CoordinateLocation]: [
+            { value: [parseFloat(stop_lat), parseFloat(stop_lon)] },
+          ],
+        },
+      };
+    })
+  );
