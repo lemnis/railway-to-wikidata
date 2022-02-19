9902f67557c69cfa743c631e6b6561e24c4afc1e
Lisa Martens <lisa@whalar.com>
2022-02-19 18:48:33 +0100

    Add extra file to cache files

---
+++ src/cache-data.ts
@@ -0,0 +1,12 @@
+import { cacheWikidataTimeZones } from "./transform/locatedInTimeZone/utils/collection";
+import { Property } from "./types/wikidata";
+import { db, dbIsLoaded } from "./utils/database";
+
+(async () => {
+    await dbIsLoaded;
+    const collection = db.getCollection(Property.LocatedInTimeZone);
+    collection.clear();
+    await cacheWikidataTimeZones()
+    db.saveDatabase();
+    db.close();
+})();
