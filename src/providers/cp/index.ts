0de1c27252c47bfca3dfa4283d19203e5e6913f1
Lisa Martens <lisa@whalar.com>
2022-02-19 19:51:24 +0100

    Remove gtfs implementation

--- src/providers/cp/index.ts
+++ src/providers/cp/index.ts
@@ -1,47 +1,19 @@
-import { Country } from "../../transform/country";
+/// <reference path="../../types/fptf.d.ts" />
+import comboios from "comboios";
+import { findCountryByAlpha2 } from "../../transform/country";
 import { LocationV4 } from "../../types/location";
 import { CodeIssuer, Property } from "../../types/wikidata";
-import { getGtfsStations } from "../../utils/gtfs";
 
-/**
- * Combios de Portugal
- * @license ?
- * @see http://dados.cm-lisboa.pt/dataset/informacao-sobre-transportes-publicos-da-cidade-de-lisboa-cp-comboios-de-portugal
- * @see https://www.transit.land/operators/o-eyc-cp
- * @see https://transitfeeds.com/p/comboios-de-portugal
- */
-export const getLocations = (): Promise<LocationV4[]> =>
-  getGtfsStations(
-    "https://www.transporlis.pt/Portals/0/OpenData/gtfs/zip/3/gtfs_3.zip",
-    "comboios-de-portugal"
-  ).then((data) =>
-    data.map(({ stop_lat, stop_lon, stop_name, stop_id }) => {
-      const stopUrl = `https://transitfeeds.com/p/comboios-de-portugal/1004/latest/stop/${stop_id}`;
-
-      return {
-        labels: [{ value: stop_name }],
-        claims: {
-          [CodeIssuer.UIC]: [
-            {
-              value: Country.Portugal.UIC[0] + stop_id.slice(3, 8),
-              references: {
-                [Property.ReferenceURL]: stopUrl,
-              },
-            },
-          ],
-          [Property.StationCode]: [
-            {
-              value: stop_id,
-              references: {
-                [Property.ReferenceURL]: stopUrl,
-              },
-            },
-          ],
-          [Property.Country]: [{ value: Country.Portugal.wikidata }],
-          [Property.CoordinateLocation]: [
-            { value: [parseFloat(stop_lat), parseFloat(stop_lon)] },
-          ],
-        },
-      };
-    })
-  );
+export const getLocations = () => comboios.stations().then((locations) =>
+  locations.map<LocationV4>(({ uicId, name, location, timezone, country }) => {
+    return {
+      labels: [{ value: name }],
+      claims: {
+        [CodeIssuer.UIC]: [{ value: uicId }],
+        [Property.LocatedInTimeZone]: [{ value: timezone }],
+        [Property.Country]: [{ value: findCountryByAlpha2(country)?.wikidata }],
+        [Property.CoordinateLocation]: [{ value: [location.latitude, location.longitude] }],
+      },
+    };
+  })
+);
