cb6cf05e61695b1dff62b0060e82ebdd616ab889
Lisa Martens <lisa@whalar.com>
2022-02-24 19:02:45 +0100

    Complete sweden geojson

--- src/providers/se-trafiklab/index.ts
+++ src/providers/se-trafiklab/index.ts
@@ -1,4 +1,4 @@
-import { TRAFIKLAB_API_KEY } from "../../../environment";
+import { TRAFIKLAB_COUNTRY_API_KEY } from "../../../environment";
 import { findCountryByUIC } from "../../transform/country";
 import { LocationV4 } from "../../types/location";
 import { CodeIssuer, Property } from "../../types/wikidata";
@@ -6,25 +6,25 @@ import { getGtfsStationsByRailRoute } from "../../utils/gtfs";
 
 export const getLocations = (): Promise<LocationV4[]> =>
   getGtfsStationsByRailRoute(
-    `https://opendata.samtrafiken.se/gtfs/sj/sj.zip?key=${TRAFIKLAB_API_KEY}`,
-    "trafilab-sj",
-    { routeType: 100 }
+    `https://api.resrobot.se/gtfs/sweden.zip?key=${TRAFIKLAB_COUNTRY_API_KEY}`,
+    "sweden",
+    { routeType: [101, 102, 105, 106] }
   ).then((data) =>
-    data
-      .map(({ stop_lat, stop_lon, stop_name, stop_id }) => {
-        const uicCountryCode = stop_id.toString().slice(5, 7);
-        const uicStationCode = stop_id.toString().slice(8, 13);
-        return {
-          labels: [{ value: stop_name }],
-          claims: {
-            [CodeIssuer.UIC]: [{ value: uicCountryCode + uicStationCode }],
-            [Property.Country]: [
-              { value: findCountryByUIC(parseInt(uicCountryCode))?.wikidata },
-            ],
-            [Property.CoordinateLocation]: [
-              { value: [parseFloat(stop_lat), parseFloat(stop_lon)] },
-            ]
-          },
-        };
-      })
+    data.map(({ stop_lat, stop_lon, stop_name, stop_id }) => {
+      const uicCountryCode = stop_id.toString().slice(0, 2);
+      const uicStationCode = stop_id.toString().slice(-5);
+      return {
+        id: stop_id,
+        labels: [{ value: stop_name }],
+        claims: {
+          [CodeIssuer.UIC]: [{ value: uicCountryCode + uicStationCode }],
+          [Property.Country]: [
+            { value: findCountryByUIC(parseInt(uicCountryCode))?.wikidata },
+          ],
+          [Property.CoordinateLocation]: [
+            { value: [parseFloat(stop_lat), parseFloat(stop_lon)] },
+          ],
+        },
+      };
+    })
   );
