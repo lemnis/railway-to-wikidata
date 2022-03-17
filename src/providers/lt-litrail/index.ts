b175e0faf422785237672bfb25a13b59a93bd380
Lisa Martens <lisa@whalar.com>
2022-03-17 12:03:20 +0100

    Add litrail

---
+++ src/providers/lt-litrail/index.ts
@@ -0,0 +1,21 @@
+import { Country } from "../../transform/country";
+import { Location } from "../../types/location";
+import { CodeIssuer, Property } from "../../types/wikidata";
+import { getStations } from "./litrail.data";
+
+export const getLocations = () =>
+  getStations().translations.map<Location>(
+    ([name, latitude, longitude, , id]) => ({
+      type: "Feature",
+      properties: {
+        id,
+        labels: [{ value: name }],
+        [Property.Country]: [{ value: Country.Lithuania.wikidata }],
+        [CodeIssuer.ESR]: [{ value: id }],
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [longitude, latitude],
+      },
+    })
+  );
---
+++ src/providers/lt-litrail/lt-litrail.test.ts
@@ -0,0 +1,41 @@
+import test from "ava";
+import fs from "fs";
+import { Location } from "../../types/location";
+import { CodeIssuer, Property } from "../../types/wikidata";
+import { Country } from "../../transform/country";
+import { getFullMatchScore } from "../../utils/test";
+import { LARGE_DATA_SIZE } from "../../score/reliability";
+
+const path = __dirname + "/../../../geojson/";
+
+const litrailStations: Location[] = JSON.parse(
+  fs.readFileSync(path + "lt-litrail.geojson", "utf-8")
+).features;
+const wikipedia: Location[] = JSON.parse(
+  fs.readFileSync(path + "uic/wikidata.geojson", "utf-8")
+).features;
+
+test("Should not have foreign locations", async (t) => {
+  t.is(
+    litrailStations.filter((feature) =>
+      feature.properties?.[Property.Country]?.every(
+        ({ value }: any) => value !== Country.Lithuania.wikidata
+      )
+    ).length,
+    0
+  );
+});
+
+test("Locations should match expected score", async (t) => {
+  const { [Property.Country]: country, [CodeIssuer.ESR]: esr } =
+    await getFullMatchScore(
+      litrailStations,
+      wikipedia,
+      [],
+      2
+    );
+
+  t.is(country.matches / country.total, 1);
+  t.is(esr?.matches / esr?.total, 1);
+  t.assert(esr?.total < LARGE_DATA_SIZE);
+});
