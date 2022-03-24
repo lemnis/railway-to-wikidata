cb900061820062a1f345ca49e48d3df043531493
Lisa Martens <lisa@whalar.com>
2022-03-24 21:40:17 +0100

    Update trafiklab

--- src/providers/se-trafiklab/index.ts
+++ src/providers/se-trafiklab/index.ts
@@ -1,27 +1,32 @@
 import { TRAFIKLAB_COUNTRY_API_KEY } from "../../../environment";
 import { findCountryByUIC } from "../../transform/country";
-import { LocationV4 } from "../../types/location";
+import { Language } from "../../transform/language";
+import { Location } from "../../types/location";
 import { CodeIssuer, Property } from "../../types/wikidata";
 import { getGtfsStationsByRailRoute } from "../../utils/gtfs";
 
-export const getLocations = (): Promise<LocationV4[]> =>
+export const getLocations = (): Promise<Location[]> =>
   getGtfsStationsByRailRoute(
     `https://api.resrobot.se/gtfs/sweden.zip?key=${TRAFIKLAB_COUNTRY_API_KEY}`,
     "sweden",
     { routeType: [101, 102, 105, 106] }
   ).then((data) =>
-    data.map(({ stop_lat, stop_lon, stop_name, stop_id }) => {
+    data.map<Location>(({ stop_lat, stop_lon, stop_name, stop_id }) => {
       const uicCountryCode = stop_id.toString().slice(0, 2);
       const uicStationCode = stop_id.toString().slice(-5);
       return {
-        id: stop_id.toString(),
-        labels: [{ value: stop_name }],
-        claims: {
+        type: "Feature",
+        id: stop_id,
+        geometry: {
+          type: "Point",
+          coordinates: [stop_lon, stop_lat],
+        },
+        properties: {
+          labels: [{ value: stop_name, lang: Language.Swedish[1] }],
           [CodeIssuer.UIC]: [{ value: uicCountryCode + uicStationCode }],
           [Property.Country]: [
             { value: findCountryByUIC(parseInt(uicCountryCode))?.wikidata },
           ],
-          [Property.CoordinateLocation]: [{ value: [stop_lat, stop_lon] }],
         },
       };
     })
---
+++ src/providers/se-trafiklab/se-trafiklab.test.ts
@@ -0,0 +1,55 @@
+import test from "ava";
+import fs from "fs";
+import { Property, CodeIssuer } from "../../types/wikidata";
+import { Country } from "../../transform/country";
+import { closeTo, getFullMatchScore } from "../../utils/test";
+import { Location } from "../../types/location";
+import { LARGE_DATA_SIZE } from "../../score/reliability";
+import { labelLanguage } from "../../utils/test/labelLanguage";
+
+const path = __dirname + "/../../../geojson/";
+
+const trafiklabLocations: Location[] = JSON.parse(
+  fs.readFileSync(path + "se-trafiklab.geojson", "utf-8")
+).features;
+const trainline: Location[] = JSON.parse(
+  fs.readFileSync(path + "wikidata-railway-stations.geojson", "utf-8")
+).features;
+
+test("Locations in Sweden should match expected score", async (t) => {
+  const { [Property.Country]: country, [CodeIssuer.UIC]: uic } =
+    await getFullMatchScore(
+      trafiklabLocations.filter((feature) =>
+        feature.properties?.[Property.Country]?.every(
+          ({ value }) => value === Country.Sweden.wikidata
+        )
+      ),
+      trainline,
+      [CodeIssuer.UIC, CodeIssuer.IBNR, Property.Country],
+      1
+    );
+
+  closeTo(t, uic?.matches / uic?.total, 1);
+  t.assert(uic?.total > LARGE_DATA_SIZE);
+  t.is(country.matches / country.total, 1);
+});
+
+test("Foreign locations should match expected score", async (t) => {
+  const { [Property.Country]: country, [CodeIssuer.UIC]: uic } =
+    await getFullMatchScore(
+      trafiklabLocations.filter((feature) =>
+        feature.properties?.[Property.Country]?.every(
+          ({ value }) => value !== Country.Sweden.wikidata
+        )
+      ),
+      trainline,
+      [CodeIssuer.UIC, Property.Country],
+      1.5
+    );
+
+  closeTo(t, uic?.matches / uic?.total, 0.95);
+  t.assert(uic?.total < LARGE_DATA_SIZE);
+  t.is(country.matches / country.total, 1);
+});
+
+test(labelLanguage, trafiklabLocations, trainline, { percentageMatch: 0.8 });
