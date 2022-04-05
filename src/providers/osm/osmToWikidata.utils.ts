de40fd3c3c9813f1d33b538a1a92e4aa52733984
Lisa Martens <lisa@whalar.com>
2022-04-05 08:47:08 +0200

    improve osm mapping

--- src/providers/osm/osmToWikidata.utils.ts
+++ src/providers/osm/osmToWikidata.utils.ts
@@ -1,47 +1,106 @@
 import { FeatureCollection, Point } from "geojson";
-import { Location } from "../../types/location";
+import { findCountryByAlpha2 } from "../../transform/country";
+import { Claims, Label, Location } from "../../types/location";
 import { ClaimObject, CodeIssuer, Items, Property } from "../../types/wikidata";
 
 const keyMap = {
+  website: Property.OfficialWebsite,
+  "contact:website": Property.OfficialWebsite,
   uic_ref: CodeIssuer.UIC,
   "ref:ibnr": CodeIssuer.IBNR,
   "ref:IBNR": CodeIssuer.IBNR,
   "railway:ref:DB": CodeIssuer.DB,
   "railway:ref:DBAG": CodeIssuer.DB,
-  "ref": Property.StationCode,
+  ref: Property.StationCode,
   "railway:ref": Property.StationCode,
-  "local_ref": Property.StationCode,
+  local_ref: Property.StationCode,
   "esr:user": CodeIssuer.ESR,
-  "wikidata": Property.Wikidata,
-  "platforms": Property.NumberOfPlatformTracks,
+  wikidata: Property.Wikidata,
+  platforms: Property.NumberOfPlatformTracks,
   iata: CodeIssuer.IATA,
 };
 
-const map = [
-  {
-    osmKey: "wheelchair",
-    osmValue: "yes",
-    wikidataKey: Property.WheelchairAccessibility,
-    wikidataValue: Items.WheelchairAccessible,
-  },
-  {
-    osmKey: "railway",
-    osmValue: "stop",
-    wikidataKey: Property.InstanceOf,
-    wikidataValue: Items.RailwayStop,
-  },
-]
+const map = {
+  wheelchair: [
+    {
+      osmValue: "yes",
+      wikidataKey: Property.WheelchairAccessibility,
+      wikidataValue: Items.WheelchairAccessible,
+    },
+    {
+      osmValue: "no",
+      wikidataKey: Property.WheelchairAccessibility,
+      wikidataValue: Items.WheelchairInaccessible,
+    },
+    {
+      osmValue: "limited",
+      wikidataKey: Property.WheelchairAccessibility,
+      wikidataValue: Items.WheelchairPartiallyAccessible,
+    },
+  ],
+  railway: [
+    {
+      osmValue: "stop",
+      wikidataKey: Property.InstanceOf,
+      wikidataValue: Items.RailwayStop,
+    },
+    {
+      osmValue: "halt",
+      wikidataKey: Property.InstanceOf,
+      wikidataValue: Items.RailwayStop,
+    },
+    {
+      osmValue: "station",
+      wikidataKey: Property.InstanceOf,
+      wikidataValue: Items.RailwayStation,
+    },
+    {
+      osmValue: "subway",
+      wikidataKey: Property.InstanceOf,
+      wikidataValue: Items.RailwayStation,
+    },
+  ],
+  subway: [
+    {
+      osmValue: "yes",
+      wikidataKey: Property.InstanceOf,
+      wikidataValue: Items.RailwayStation,
+    },
+  ],
+  internet_access: [
+    {
+      osmValue: "no",
+      wikidataKey: Property.WifiAccess,
+      wikidataValue: Items.No,
+    },
+    {
+      osmValue: "wlan",
+      wikidataKey: Property.WifiAccess,
+      wikidataValue: Items.Yes,
+    },
+  ],
+};
 
 export function osmToWikidata(
   data: FeatureCollection<Point, Record<string, string>>
 ) {
   const features = data.features.map<Location>((feature) => {
-    const { name, uic_name, ...extra } = Object.assign({}, feature.properties);
+    // skip id
+    const {
+      id,
+      name,
+      uic_name,
+      official_name,
+      old_name,
+      alt_name,
+      train,
+      public_transport,
+      "addr:country": country,
+      ...extra
+    } = Object.assign({}, feature.properties);
 
-    const properties: Record<CodeIssuer | Property, ClaimObject<string>[]> =
-      {} as any;
-
-    const labels = [];
+    const properties: Claims = {};
+    const labels: Label[] = [];
 
     Object.entries(extra).map(([key, value]) => {
       // todo old_name
@@ -56,10 +115,47 @@ export function osmToWikidata(
         properties[keyMap[key as keyof typeof keyMap]]!.push({ value });
         delete extra[key];
       }
+      if (key in map) {
+        const wiki = map[key as keyof typeof map].find(
+          ({ osmValue }) => osmValue === value
+        );
+        if (wiki) {
+          properties[wiki.wikidataKey] ||= [];
+          properties[wiki.wikidataKey]!.push({ value: wiki.wikidataValue });
+          delete extra[key];
+        }
+      }
     });
 
-    if (name) labels.push({ value: name });
-    if (uic_name) labels.push({ value: uic_name });
+    [
+      ...new Set(
+        [name, uic_name, official_name, alt_name, old_name].filter(Boolean)
+      ),
+    ].forEach((value) => labels.push({ value }));
+
+    if (country) {
+      properties[Property.Country] ||= [];
+      properties[Property.Country]?.push({
+        value: findCountryByAlpha2(country)?.wikidata,
+      });
+    }
+
+    if (train === "yes" && !properties[Property.InstanceOf]?.length) {
+      properties[Property.InstanceOf] ||= [];
+      properties[Property.InstanceOf]?.push({
+        value: Items.RailwayStation,
+      });
+    }
+
+    if (
+      public_transport === "stations" &&
+      !properties[Property.InstanceOf]?.length
+    ) {
+      properties[Property.InstanceOf] ||= [];
+      properties[Property.InstanceOf]?.push({
+        value: Items.RailwayStation,
+      });
+    }
 
     return {
       ...feature,
