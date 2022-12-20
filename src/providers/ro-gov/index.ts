8e808c97f3026f11c421b08f1b7293246015a82b
Lisa Martens <lemnis.martens@gmail.com>
2022-12-20 01:55:38 +0100

    Remove duplicates

--- src/providers/ro-gov/index.ts
+++ src/providers/ro-gov/index.ts
@@ -1,7 +1,15 @@
 import fetch from "node-fetch";
 import { CodeIssuer, Property } from "../../types/wikidata";
 import { Basic, Claims, Location } from "../../types/location";
-import { feature, FeatureCollection, point, Point } from "@turf/turf";
+import {
+  combine,
+  feature,
+  featureCollection,
+  FeatureCollection,
+  multiPoint,
+  point,
+  Point,
+} from "@turf/turf";
 import { Country, findCountryByAlpha2 } from "../../transform/country";
 import { feature as c } from "@ideditor/country-coder";
 
@@ -14,27 +22,41 @@ export const getLocations = async () => {
       "https://raw.githubusercontent.com/vasile/data.gov.ro-gtfs-exporter/master/cfr.webgis.ro/stops.geojson"
     ).then((response) => response.json());
 
-  return data.features.map<Location>(({ properties, geometry }) => {
-    const country = c(geometry.coordinates as any)?.properties;
+  return data.features
+    .map<Location>(({ properties, geometry }) => {
+      const country = c(geometry.coordinates as any)?.properties;
 
-    return feature<Point, Claims & Basic>(
-      geometry,
-      {
-        labels: properties.name ? [{ value: properties.name }] : [],
-        [Property.Country]: [{ value: country?.wikidata }],
-        ...(country?.iso1A2 === Country.Romania.alpha2
-          ? {
-              [CodeIssuer.UIC]: [
-                {
-                  value:
-                    findCountryByAlpha2(country?.iso1A2)?.UIC?.[0] +
-                    properties.station_id,
-                },
-              ],
-            }
-          : {}),
-      },
-      { id: properties.station_id }
-    );
-  });
+      return feature<Point, Claims & Basic>(
+        geometry,
+        {
+          labels: properties.name ? [{ value: properties.name }] : [],
+          [Property.Country]: [{ value: country?.wikidata }],
+          ...(country?.iso1A2 === Country.Romania.alpha2
+            ? {
+                [CodeIssuer.UIC]: [
+                  {
+                    value:
+                      findCountryByAlpha2(country?.iso1A2)?.UIC?.[0] +
+                      properties.station_id,
+                  },
+                ],
+              }
+            : {}),
+        },
+        { id: properties.station_id }
+      );
+    })
+    .reduce<any[]>((res, a) => {
+      const index = res.findIndex((b) => a.id === b.id);
+      if (index > -1) {
+        res[index] = multiPoint(
+          [res[index].geometry.coordinates, a.geometry.coordinates],
+          res[index].properties,
+          { id: res[index].id }
+        );
+      } else {
+        res.push(a);
+      }
+      return res;
+    }, []);
 };
