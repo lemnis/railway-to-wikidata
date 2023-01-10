4b37659a450be67185df425f01d94aea50891fb4
Lisa Martens <lemnis.martens@gmail.com>
2023-01-10 13:07:13 +0100

    Merge duplicate peatus locations

--- src/providers/ee-peatus/index.ts
+++ src/providers/ee-peatus/index.ts
@@ -3,6 +3,8 @@ import { Location } from "../../types/location";
 import { Country } from "../../transform/country";
 import { getGtfsStationsByRailRoute } from "../../utils/gtfs";
 import { point } from "@turf/turf";
+import { score } from "../../score";
+import { merge } from "../../actions/merge";
 
 /**
  * @license Unknown
@@ -14,7 +16,7 @@ export const getLocations = async (): Promise<Location[]> => {
     "peatus"
   );
 
-  return data
+  const ungroupedStations = data
     .map<Location>(({ stop_lat, stop_lon, stop_name, stop_id }) =>
       point(
         [stop_lon, stop_lat],
@@ -26,4 +28,33 @@ export const getLocations = async (): Promise<Location[]> => {
       )
     )
     .sort((a, b) => a.id!.toString().localeCompare(b.id!.toString()));
+
+  const groupedStations: Location[][] = [];
+
+  for await (const station of ungroupedStations) {
+    const [index, highestMatch] =
+      (await Promise.all(
+        groupedStations.map((r, index) =>
+          Promise.all(r.map((b) => score(station, b)))
+            .then((r) => r.sort((a, b) => b.percentage - a.percentage)?.[0])
+            .then(
+              (r) => [index, r] as [number, Awaited<ReturnType<typeof score>>]
+            )
+        )
+      ).then(
+        (r) => r.sort((a, b) => b[1].percentage - a[1].percentage)?.[0]
+      )) || [];
+
+    if (highestMatch?.percentage >= 2) {
+      groupedStations[index].push(station);
+    } else {
+      groupedStations.push([station]);
+    }
+  }
+
+  return await Promise.all(
+    groupedStations.map((stations) =>
+      stations.length > 1 ? merge(stations, false, false) : stations[0]
+    )
+  );
 };
